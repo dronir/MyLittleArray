f04a22b015bc40f356344b71d1299abadb4d0bea
Olli Wilkman <olli.wilkman@iki.fi>
2013-03-28 17:01:54 +0200

    Fix bugs and add a couple of missing features

--- src/MyLittleArray.jl
+++ src/MyLittleArray.jl
@@ -7,15 +7,18 @@ abstract AbstractLittleMatrix
 
 include("dimension3.jl")
 include("dimension2.jl")
+include("dimension4.jl")
 
 unit(V::AbstractLittleVector) = V / norm(V)
 
-MyVector(a,b) = MyVector2(a,b)
-MyVector(a,b,c) = MyVector3(a,b,c)
+MyLittleVector(a,b) = MyLittleVector2(a,b)
+MyLittleVector(a,b,c) = MyLittleVector3(a,b,c)
+MyLittleVector(a,b,c,d) = MyLittleVector4(a,b,c,d)
 
 export AbstractLittleVector, AbstractLittleMatrix
-export MyVector
-export MyVector3, MyMatrix3, MyDiagonal3
-export MyVector2, MyMatrix2, MyDiagonal2
+export MyLittleVector
+export MyLittleVector2, MyLittleMatrix2, MyLittleDiagonal2
+export MyLittleVector3, MyLittleMatrix3, MyLittleDiagonal3
+export MyLittleVector4, MyLittleMatrix4, MyLittleDiagonal4
 
 end # module
--- src/dimension2.jl
+++ src/dimension2.jl
@@ -1,160 +1,160 @@
-immutable MyVector2 <: AbstractLittleVector
+immutable MyLittleVector2 <: AbstractLittleVector
     e1::Float64
     e2::Float64
 end
 
-immutable MyMatrix2 <: AbstractLittleMatrix
+immutable MyLittleMatrix2 <: AbstractLittleMatrix
     e11::Float64
     e12::Float64
     e21::Float64
     e22::Float64
 end
 
-immutable MyDiagonal2 <: AbstractLittleMatrix
+immutable MyLittleDiagonal2 <: AbstractLittleMatrix
     e11::Float64
     e22::Float64
 end
 
 
-MyMatrix2(A::MyDiagonal2) = MyMatrix2(A.e11,0.0,0.0,A.e22)
-MyDiagonal2(V::MyVector2) = MyDiagonal2(V.e1, V.e2)
+MyLittleMatrix2(A::MyLittleDiagonal2) = MyLittleMatrix2(A.e11,0.0,0.0,A.e22)
+MyLittleDiagonal2(V::MyLittleVector2) = MyLittleDiagonal2(V.e1, V.e2)
 
 
-function getindex(V::MyVector2, i::Integer)
+function getindex(V::MyLittleVector2, i::Integer)
     if i==1
         return V.e1
     elseif i==2
         return V.e2
     else
-        error("Invalid index")    
+        throw(BoundsError)    
     end
 end
 
-function getindex(A::MyMatrix2, i::Integer, j::Integer)
+function getindex(A::MyLittleMatrix2, i::Integer, j::Integer)
     if i==1
-        return j==1 ? A.e11 : j==2 ? A.e12 : error("Invalid index")
+        return j==1 ? A.e11 : j==2 ? A.e12 : throw(BoundsError)
     elseif i==2
-        return j==1 ? A.e21 : j==2 ? A.e22 : error("Invalid index")
+        return j==1 ? A.e21 : j==2 ? A.e22 : throw(BoundsError)
     else
-        error("Invalid index")    
+        throw(BoundsError)    
     end
 end
 
-function getindex(A::MyDiagonal2, i::Integer, j::Integer)
+function getindex(A::MyLittleDiagonal2, i::Integer, j::Integer)
     if i==1
-        return j==1 ? A.e11 : j==2 ? 0.0 : error("Invalid index")
+        return j==1 ? A.e11 : j==2 ? 0.0 : throw(BoundsError)
     elseif i==2
-        return j==1 ? 0.0 : j==2 ? A.e22 : error("Invalid index")
+        return j==1 ? 0.0 : j==2 ? A.e22 : throw(BoundsError)
     else
-        error("Invalid index")    
+        throw(BoundsError)    
     end
 end
 
 
-size(V::MyVector2) = (2,)
-size(A::MyDiagonal2) = (2,2)
-size(A::MyMatrix2) = (2,2)
-size(V::MyVector2, N::Integer) = N==1 ? 2 : error("Invalid dimension")
-size(A::MyDiagonal2, N::Integer) = N==1 || N==2 ? 2 : error("Invalid dimension")
-size(A::MyMatrix2, N::Integer) = N==1 || N==2 ? 2 : error("Invalid dimension")
-length(V::MyVector2) = 2
-length(A::MyDiagonal2) = 4
-length(A::MyMatrix2) = 4
+size(V::MyLittleVector2) = (2,)
+size(A::MyLittleDiagonal2) = (2,2)
+size(A::MyLittleMatrix2) = (2,2)
+size(V::MyLittleVector2, N::Integer) = N==1 ? 2 : error("Invalid dimension")
+size(A::MyLittleDiagonal2, N::Integer) = N==1 || N==2 ? 2 : error("Invalid dimension")
+size(A::MyLittleMatrix2, N::Integer) = N==1 || N==2 ? 2 : error("Invalid dimension")
+length(V::MyLittleVector2) = 2
+length(A::MyLittleDiagonal2) = 4
+length(A::MyLittleMatrix2) = 4
 
 
 # Functions to make new things
 
-zeros(T::Type{MyVector2}) = MyVector2(0.0, 0.0)
-zeros(T::Type{MyDiagonal2}) = MyDiagonal2(0.0, 0.0)
-zeros(T::Type{MyMatrix2}) = MyMatrix2(0.0, 0.0, 0.0, 0.0)
+zeros(T::Type{MyLittleVector2}) = MyLittleVector2(0.0, 0.0)
+zeros(T::Type{MyLittleDiagonal2}) = MyLittleDiagonal2(0.0, 0.0)
+zeros(T::Type{MyLittleMatrix2}) = MyLittleMatrix2(0.0, 0.0, 0.0, 0.0)
 
-eye(T::Type{MyDiagonal2}) = MyDiagonal2(1.0,1.0)
-eye(T::Type{MyMatrix2}) = MyMatrix2(1.0,0.0,0.0,1.0)
+eye(T::Type{MyLittleDiagonal2}) = MyLittleDiagonal2(1.0,1.0)
+eye(T::Type{MyLittleMatrix2}) = MyLittleMatrix2(1.0,0.0,0.0,1.0)
 
 function Rot(theta::Real) 
     sT = sin(theta)
     cT = cos(theta)
-    MyMatrix2(cT, -sT, sT, cT)
+    MyLittleMatrix2(cT, -sT, sT, cT)
 end
 
 
 
 
 # Vector functions
-norm(V::MyVector2) = sqrt(V.e1^2 + V.e2^2)
-dot(A::MyVector2, B::MyVector2) = A.e1*B.e1 + A.e2*B.e2
+norm(V::MyLittleVector2) = sqrt(V.e1^2 + V.e2^2)
+dot(A::MyLittleVector2, B::MyLittleVector2) = A.e1*B.e1 + A.e2*B.e2
 
 
 # Matrix functions
-transpose(A::MyDiagonal2) = A
-transpose(A::MyMatrix2) = MyMatrix2(A.e11, A.e21, A.e12, A.e22)
+transpose(A::MyLittleDiagonal2) = A
+transpose(A::MyLittleMatrix2) = MyLittleMatrix2(A.e11, A.e21, A.e12, A.e22)
 
 # Unary operators
--(V::MyVector2) = MyVector2(-V.e1, -V.e2)
--(A::MyDiagonal2) = MyDiagonal2(-A.e11, -A.e22)
--(A::MyMatrix2) = MyMatrix2(-A.e11,-A.e12,-A.e21,-A.e22)
+-(V::MyLittleVector2) = MyLittleVector2(-V.e1, -V.e2)
+-(A::MyLittleDiagonal2) = MyLittleDiagonal2(-A.e11, -A.e22)
+-(A::MyLittleMatrix2) = MyLittleMatrix2(-A.e11,-A.e12,-A.e21,-A.e22)
 
 
 # Scalar-vector operations
-*(a::Real, V::MyVector2) = MyVector2(a*V.e1, a*V.e2)
-*(V::MyVector2, a::Real) = *(a,V)
-+(a::Real, V::MyVector2) = MyVector2(a+V.e1, a+V.e2)
-+(V::MyVector2, a::Real) = +(a,V)
--(a::Real, V::MyVector2) = MyVector2(a-V.e1, a-V.e2)
--(V::MyVector2, a::Real) = MyVector2(V.e1-a, V.e2-a)
-/(V::MyVector2, a::Real) = MyVector2(V.e1/a, V.e2/a)
-./(a::Real, V::MyVector2) = MyVector2(a/V.e1, a/V.e2)
-.^(V::MyVector2, a::Real) = MyVector2(V.e1^a, V.e2^a)
-.^(a::Real, V::MyVector2) = MyVector2(a^V.e1, a^V.e2)
+*(a::Real, V::MyLittleVector2) = MyLittleVector2(a*V.e1, a*V.e2)
+*(V::MyLittleVector2, a::Real) = *(a,V)
++(a::Real, V::MyLittleVector2) = MyLittleVector2(a+V.e1, a+V.e2)
++(V::MyLittleVector2, a::Real) = +(a,V)
+-(a::Real, V::MyLittleVector2) = MyLittleVector2(a-V.e1, a-V.e2)
+-(V::MyLittleVector2, a::Real) = MyLittleVector2(V.e1-a, V.e2-a)
+/(V::MyLittleVector2, a::Real) = MyLittleVector2(V.e1/a, V.e2/a)
+./(a::Real, V::MyLittleVector2) = MyLittleVector2(a/V.e1, a/V.e2)
+.^(V::MyLittleVector2, a::Real) = MyLittleVector2(V.e1^a, V.e2^a)
+.^(a::Real, V::MyLittleVector2) = MyLittleVector2(a^V.e1, a^V.e2)
 
 # Scalar-diagonal operations
-*(a::Real, V::MyDiagonal2) = MyDiagonal2(a*V.e1, a*V.e2)
-*(V::MyDiagonal2, a::Real) = *(a,V)
-+(a::Real, V::MyDiagonal2) = MyDiagonal2(a+V.e1, a+V.e2)
-+(V::MyDiagonal2, a::Real) = +(a,V)
--(a::Real, V::MyDiagonal2) = MyDiagonal2(a-V.e1, a-V.e2)
--(V::MyDiagonal2, a::Real) = MyDiagonal2(V.e1-a, V.e2-a)
-/(V::MyDiagonal2, a::Real) = MyDiagonal2(V.e1/a, V.e2/a)
-./(a::Real, V::MyDiagonal2) = MyDiagonal2(a/V.e1, a/V.e2)
-.^(V::MyDiagonal2, a::Real) = MyDiagonal2(V.e1^a, V.e2^a)
-.^(a::Real, V::Matrix) = MyDiagonal2(a^V.e1, a^V.e2)
+*(a::Real, V::MyLittleDiagonal2) = MyLittleDiagonal2(a*V.e1, a*V.e2)
+*(V::MyLittleDiagonal2, a::Real) = *(a,V)
++(a::Real, V::MyLittleDiagonal2) = MyLittleDiagonal2(a+V.e1, a+V.e2)
++(V::MyLittleDiagonal2, a::Real) = +(a,V)
+-(a::Real, V::MyLittleDiagonal2) = MyLittleDiagonal2(a-V.e1, a-V.e2)
+-(V::MyLittleDiagonal2, a::Real) = MyLittleDiagonal2(V.e1-a, V.e2-a)
+/(V::MyLittleDiagonal2, a::Real) = MyLittleDiagonal2(V.e1/a, V.e2/a)
+./(a::Real, V::MyLittleDiagonal2) = MyLittleDiagonal2(a/V.e1, a/V.e2)
+.^(V::MyLittleDiagonal2, a::Real) = MyLittleDiagonal2(V.e1^a, V.e2^a)
+.^(a::Real, V::Matrix) = MyLittleDiagonal2(a^V.e1, a^V.e2)
 
 
 # Matrix-vector products
 
-*(A::MyDiagonal2, V::MyVector2) = MyVector2(A.e11*V.e1, A.e22*V.e2)
+*(A::MyLittleDiagonal2, V::MyLittleVector2) = MyLittleVector2(A.e11*V.e1, A.e22*V.e2)
 
-function *(A::MyMatrix2, V::MyVector2)
+function *(A::MyLittleMatrix2, V::MyLittleVector2)
     v1 = A.e11 * V.e1 + A.e12 * V.e2
     v2 = A.e21 * V.e1 + A.e22 * V.e2
-    return MyVector2(v1, v2)
+    return MyLittleVector2(v1, v2)
 end
 
 
 # Matrix-matrix products
 
-*(A::MyDiagonal2, B::MyDiagonal2) = MyDiagonal2(A.e11*B.e11, A.e22*B.e22)
+*(A::MyLittleDiagonal2, B::MyLittleDiagonal2) = MyLittleDiagonal2(A.e11*B.e11, A.e22*B.e22)
 
-function *(A::MyMatrix2, B::MyDiagonal2)
+function *(A::MyLittleMatrix2, B::MyLittleDiagonal2)
     c11 = A.e11 * B.e11
     c12 = A.e12 * B.e22
     c21 = A.e21 * B.e11
     c22 = A.e22 * B.e22
-    return MyMatrix2(c11,c12,c21,c22)
+    return MyLittleMatrix2(c11,c12,c21,c22)
 end
 
-function *(A::MyMatrix2, B::MyDiagonal2)
+function *(A::MyLittleMatrix2, B::MyLittleDiagonal2)
     c11 = A.e11 * B.e11
     c12 = A.e11 * B.e12
     c21 = A.e22 * B.e21
     c22 = A.e22 * B.e22
-    return MyMatrix2(c11,c12,c21,c22)
+    return MyLittleMatrix2(c11,c12,c21,c22)
 end
 
-function *(A::MyMatrix2, B::MyMatrix2)
+function *(A::MyLittleMatrix2, B::MyLittleMatrix2)
     c11 = A.e11 * B.e11  +  A.e12 * B.e21
     c12 = A.e11 * B.e12  +  A.e12 * B.e22
     c21 = A.e21 * B.e11  +  A.e22 * B.e21
     c22 = A.e21 * B.e12  +  A.e22 * B.e22
-    return MyMatrix2(c11,c12,c21,c22)
+    return MyLittleMatrix2(c11,c12,c21,c22)
 end
